f73e13f62822447e9a919d158e5c40e6c74c5ca3
k-miyake <3katsu3hiro@gmail.com>
2014-05-31 21:54:59 +0900

    5章ソフトウェアで表現されたモデル エンティティ 新規作成

---
+++ src/chapter5_model_rendering_by_software/subtile_entity/domain/Customer.java
@@ -0,0 +1,10 @@
+package chapter5_model_rendering_by_software.subtile_entity.domain;
+
+public class Customer {
+	private String id;
+	private String name;
+	private Integer saleVolumeAverage;
+	private String merchandiseCategory;
+	private SalesContactInfo salesContactInfo; 
+	
+}
---
+++ src/chapter5_model_rendering_by_software/subtile_entity/domain/SalesContactInfo.java
@@ -0,0 +1,8 @@
+package chapter5_model_rendering_by_software.subtile_entity.domain;
+
+public class SalesContactInfo {
+	private String salesMan;
+	private Integer priority;
+	private String telephoneNumber;
+	private String address;
+}
