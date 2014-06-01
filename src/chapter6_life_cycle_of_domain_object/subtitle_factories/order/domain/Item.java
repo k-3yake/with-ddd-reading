8a927d53bdf2636ab59d0a6855ea38ac1227888c
k-miyake <3katsu3hiro@gmail.com>
2014-06-01 16:48:25 +0900

    6章ドメインオブジェクトのライフサイクル ファクトリ ファクトリを使わないパターン

--- src/chapter6_life_cycle_of_domain_object/subtitle_factories/order/domain/Item.java
+++ src/chapter6_life_cycle_of_domain_object/subtitle_factories/order/domain/Item.java
@@ -1,10 +1,12 @@
 package chapter6_life_cycle_of_domain_object.subtitle_factories.order.domain;
 
 public class Item implements Value{
+	private String itemNo;
 	private Integer price;
 
-	public Item(Integer price) {
+	public Item(String itemNo,Integer price) {
 		super();
+		this.itemNo = itemNo;
 		this.price = price;
 	}
 
