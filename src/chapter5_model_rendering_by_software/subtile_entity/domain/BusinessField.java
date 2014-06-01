b67b34a269b1d5194c2e8e6f67c94fe563622531
k-miyake <3katsu3hiro@gmail.com>
2014-06-01 10:57:10 +0900

    5章ソフトウェアで表現されたモデル エンティティ 顧客から事業分野に顧客の同一性に関連しないフィールドを移動

---
+++ src/chapter5_model_rendering_by_software/subtile_entity/domain/BusinessField.java
@@ -0,0 +1,7 @@
+package chapter5_model_rendering_by_software.subtile_entity.domain;
+
+public class BusinessField {
+	private Integer saleVolumeAverage;
+	private String merchandiseCategory;
+
+}
--- src/chapter5_model_rendering_by_software/subtile_entity/domain/Customer.java
+++ src/chapter5_model_rendering_by_software/subtile_entity/domain/Customer.java
@@ -3,8 +3,8 @@ package chapter5_model_rendering_by_software.subtile_entity.domain;
 public class Customer {
 	private String id;
 	private String name;
-	private Integer saleVolumeAverage;
-	private String merchandiseCategory;
+	private String telephoneNumber;
+	private String address;
+	private BusinessField bsinessField;
 	private SalesContactInfo salesContactInfo; 
-	
 }
